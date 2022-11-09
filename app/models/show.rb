da7653bbca0dcd2ac3f0b07f8953cc6b21b0fbd1
erickmuriithi <rickymurithii@gmail.com>
2022-11-09 12:17:44 +0300

    add file

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
     has_many :characters
         has_many :actors, through: :characters
         belongs_to :network
-  
+    
     
         def actors_list
             self.actors.map do |actor|
