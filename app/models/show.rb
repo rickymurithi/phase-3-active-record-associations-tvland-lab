ddef51b2aa3dba805d2545800a3424e59abdc6ed
erickmuriithi <rickymurithii@gmail.com>
2022-11-09 16:49:56 +0300

    add files

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,13 +1,11 @@
 class Show < ActiveRecord::Base
     has_many :characters
-        has_many :actors, through: :characters
-        belongs_to :network
-    
-    
-        def actors_list
-            self.actors.map do |actor|
-                "#{actor.first_name} #{actor.last_name}"
-            end
-        end   
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.actors.map do |actor|
+            "#{actor.first_name} #{actor.last_name}"
+        end
     end
 end
